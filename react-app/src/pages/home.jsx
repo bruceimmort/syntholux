c20085018ac4f3b4b3e8092e3a49075f22ca191c
Aime_Bruce <98209400+bruceimmort@users.noreply.github.com>
2024-06-24 17:03:53 +0200

    frontend: music player (song info)

--- react-app/src/pages/home.jsx
+++ react-app/src/pages/home.jsx
@@ -1,8 +1,33 @@
+import "../assets/css/pages/home.css"
+import icons from "../utils/icons";
+
 export default function Home(){
     return(
         <>
-        <h2>Syntholux <br/> </h2> 
-        <p>Create and control ur taste in music</p>
+        <div className="all">
+            <div className="containerDiv"></div>
+            <div className="musicPlayerDiv">
+                <div className="musicInfoDiv">
+                    <div className="infoDiv">
+                        <div className="songName"> What was I made for</div>
+                        <div className="artistName"> Billie Eilish</div>
+                    </div>
+                    <div className="likeSongDiv">
+                        <img src={icons.heart} alt="" />
+                    </div>
+                </div>
+                <div className="controlsDiv">
+                    <div className="mainControlsDiv">
+                        <div className="timeStampsDiv">
+                            <div className="startingTime">00:00</div>
+                            <div className="timelineBar"></div>
+                            <div className="startingTime">04:00</div>
+                        </div>
+                    </div>
+                    <div className="flowControlsDiv"></div>
+                </div>
+            </div>
+        </div>
         </>
     );
 }
