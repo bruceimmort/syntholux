3143ed4047a5c8c5df78b23a95c8535cb3cd5ca3
Aime_Bruce <98209400+bruceimmort@users.noreply.github.com>
2024-06-25 17:01:20 +0200

    frontend: bottom music player

--- react-app/src/pages/home.jsx
+++ react-app/src/pages/home.jsx
@@ -1,30 +1,51 @@
+import { useState } from "react";
 import "../assets/css/pages/home.css"
 import icons from "../utils/icons";
 
 export default function Home(){
+    const [playing, setPlaying] = useState(true);
     return(
         <>
         <div className="all">
             <div className="containerDiv"></div>
+            {/* Bottom music player */}
             <div className="musicPlayerDiv">
+                {/* Music name and artist */}
                 <div className="musicInfoDiv">
                     <div className="infoDiv">
                         <div className="songName"> What was I made for</div>
                         <div className="artistName"> Billie Eilish</div>
                     </div>
                     <div className="likeSongDiv">
-                        <img src={icons.heart} alt="" />
+                        <img src={icons.heartIcon} alt="" />
                     </div>
                 </div>
                 <div className="controlsDiv">
+                    {/* Music player contro;s */}
                     <div className="mainControlsDiv">
                         <div className="timeStampsDiv">
                             <div className="startingTime">00:00</div>
                             <div className="timelineBar"></div>
                             <div className="startingTime">04:00</div>
                         </div>
+                        <div className="flowControlsDiv">
+                            <img src={icons.prevIcon} alt="" title="Previous"/>
+                            <div className="play/play" title="Play / Pause">
+                                {playing  ? 
+                                <img src={icons.pausedIcon} alt=""/>
+                                : 
+                                <img src={icons.heartIcon} alt=""/>}
+                            </div>
+                            <img src={icons.nextIcon} alt="" title="Next"/>
+                        </div>
                     </div>
-                    <div className="flowControlsDiv"></div>
+                    <div className="otherControlsDiv">
+                        <div className="otherControlsContainer">
+                            <img src={icons.repeatIcon} alt="" />
+                            <img src={icons.speakerIcon} alt="" />
+                            <img src={icons.expandIcon} alt="" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- react-app/src/utils/icons.jsx
+++ react-app/src/utils/icons.jsx
@@ -1,5 +1,17 @@
-import heart from "../assets/icons/heart.png"
+import heartIcon from "../assets/icons/heart.png"
+import prevIcon from "../assets/icons/prev-btn.png"
+import nextIcon from "../assets/icons/next-btn.png"
+import pausedIcon from "../assets/icons/paused-btn.png"
+import repeatIcon from "../assets/icons/repeat.png"
+import speakerIcon from "../assets/icons/mute.png"
+import expandIcon from "../assets/icons/expand.png"
 
 export default {
-    heart,
+    heartIcon,
+    prevIcon,
+    nextIcon,
+    pausedIcon,
+    repeatIcon,
+    speakerIcon,
+    expandIcon
 }
